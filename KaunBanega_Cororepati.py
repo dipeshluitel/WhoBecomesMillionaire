53daadf9729ecf53019c146a7d88722d45854aa9
DIPESH <dipeshluitel40@gmail.com>
2023-06-04 18:53:22 +0545

    Answers for the 10 questions are added in list 'ans'

--- KaunBanega_Cororepati.py
+++ KaunBanega_Cororepati.py
@@ -1,2 +1,3 @@
 # source = https://www.edudwar.com/world-gk-quiz-questions-and-answers/
 questions = ["Which is the Smallest ocean?", "Which country gifted 'Statue of Liberty' to USA", "Which Country is also known as 'Land of Rising Sun'?","Which Continent has the highest number of countries?", "Total number of ocean in the world is?","Which is one of the biggest island?","The world's longest straight road without any corners is located in?","Which one is the largest tropical rain forest in the world?","Which country is known as 'Land of Thousands Lakes'?","In which country, white elephant is found?"]
+ans=["Arctic","France","Japan","Africa","5","Greenland","Saudi Arabia","Amazon","Finland","Thailand"]
